360d06cbcbdbe8eb142bd9155bca73871005f06b
brian91292 <brian@yur.fit>
2020-06-09 19:36:09 -0500

    add better error handling to mixer authed http client

--- ChatCore/Services/Mixer/MixerAuthedHttpClient.cs
+++ ChatCore/Services/Mixer/MixerAuthedHttpClient.cs
@@ -23,9 +23,11 @@ namespace ChatCore.Services.Mixer
         private HttpClient _httpClient;
         private IUserAuthProvider _authProvider;
 
-        private async Task AddAuthHeader(HttpRequestMessage request)
+        private async Task<bool> AddAuthHeader(HttpRequestMessage request)
         {
-            if (_authProvider.Credentials.Mixer_ExpiresAt <= DateTime.UtcNow.AddMinutes(1))
+            bool accessTokenExists = !string.IsNullOrWhiteSpace(_authProvider.Credentials.Mixer_AccessToken);
+            bool refreshTokenExists = !string.IsNullOrWhiteSpace(_authProvider.Credentials.Mixer_RefreshToken);
+            if ((accessTokenExists && _authProvider.Credentials.Mixer_ExpiresAt <= DateTime.UtcNow.AddMinutes(1)) || (!accessTokenExists && refreshTokenExists))
             {
                 _logger.LogInformation($"Refreshing Mixer auth token!");
                 await _authProvider.TryRefreshMixerCredentials();
@@ -34,19 +36,31 @@ namespace ChatCore.Services.Mixer
             {
                 _logger.LogInformation($"Auth token expires in {(_authProvider.Credentials.Mixer_ExpiresAt - DateTime.UtcNow.AddMinutes(1)).ToString()}, Adding mixer auth header!");
                 request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _authProvider.Credentials.Mixer_AccessToken);
+                return true;
             }
+            return false;
         }
 
         public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
         {
-            await AddAuthHeader(request);
-            return await _httpClient.SendAsync(request);
+            if (await AddAuthHeader(request))
+            {
+                return await _httpClient.SendAsync(request);
+            }
+            var resp = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            resp.Content = new StringContent("Mixer authentication credentials are required to complete the request.");
+            return resp;
         }
 
         public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            await AddAuthHeader(request);
-            return await _httpClient.SendAsync(request, cancellationToken);
+            if (await AddAuthHeader(request))
+            {
+                return await _httpClient.SendAsync(request, cancellationToken);
+            }
+            var resp = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            resp.Content = new StringContent("Mixer authentication credentials are required to complete the request.");
+            return resp;
         }
     }
 }
